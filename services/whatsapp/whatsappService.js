8edd333ca2a245c41421629804a66a42343b178a
Michael Sonwu <michael.sonwu.careers@gmail.com>
2026-07-22 08:26:37 +0100

    Log WhatsApp API errors

--- services/whatsapp/whatsappService.js
+++ services/whatsapp/whatsappService.js
@@ -4,20 +4,30 @@ const TOKEN = process.env.WHATSAPP_ACCESSS_TOKEN;
 const PHONE_ID = process.env.WHATSAPP_PHONE_NUMBER_ID;
 
 async function sendMessage(to, payload) {
-  return axios.post(
-    `https://graph.facebook.com/v23.0/${PHONE_ID}/messages`,
-    {
-      messaging_product: "whatsapp",
-      to,
-      ...payload
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-        "Content-Type": "application/json"
+  try {
+    const res = await axios.post(
+      `https://graph.facebook.com/v23.0/${PHONE_ID}/messages`,
+      {
+        messaging_product: "whatsapp",
+        to,
+        ...payload
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json"
+        }
       }
-    }
-  );
+    );
+
+    console.log("WHATSAPP SUCCESS:", res.data);
+    return res.data;
+
+  } catch (err) {
+    console.log("WHATSAPP ERROR:");
+    console.log(err.response?.data || err.message);
+    throw err;
+  }
 }
 
 module.exports = {
